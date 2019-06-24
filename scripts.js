9b51c0ff643e55cc68b777249e0feb558f534607
erwin snijder <erwinsnijder_4@hotmail.com>
2019-06-24 12:17:41 +0200

    update

--- scripts.js
+++ scripts.js
@@ -19,6 +19,7 @@ var data = JSON.parse(this.response)
 
 if (request.status >= 200 && request.status < 400) {
 data.forEach(movie => {
+  // console.table(movie.director, movie.rt_score)
 const card = document.createElement('div')
 card.setAttribute('class', 'card')
 
@@ -26,11 +27,18 @@ const h1 = document.createElement('h1')
 h1.textContent = movie.title
 
 const p = document.createElement('p')
-movie.description = movie.description.substring(0, 300)
+movie.description = movie.description.substring(0, 400)
 p.textContent = `${movie.description}...`
+
+const h2 = document.createElement('h2')
+h2.textContent = movie.producer
+const h3 = document.createElement('h3')
+h3.textContent = movie.release_date + ', ' + `${movie.rt_score}%` 
 
 container.appendChild(card)
 card.appendChild(h1)
+card.appendChild(h2)
+card.appendChild(h3)
 card.appendChild(p)
 })
 
