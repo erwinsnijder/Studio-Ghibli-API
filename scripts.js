75a5fb2d11b6f0d53d0b77017cbe35c2ac983863
erwin snijder <erwinsnijder_4@hotmail.com>
2019-06-24 10:40:34 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+const app = document.getElementById('root')
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(logo)
+app.appendChild(container)
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
 
@@ -8,11 +17,20 @@ request.onload = function () {
 // Begin accessing JSON data here
 var data = JSON.parse(this.response)
 
+if (request.status >= 200 && request.status < 400){
 data.forEach(movie => {
+
+  // Create a div with a card class
+  const card = createElement('div')
+  card.setAttribute('class', 'card')
   // Log each movie's title
   console.log(movie.title)
+  console.log(movie.description)
 })
+} else
+{
+  console.log('error')
 }
-
+}
 // Send request
 request.send()
